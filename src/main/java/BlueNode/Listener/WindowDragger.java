558fd64380818ffea33a039897f8d39ea5498ce3
Aram <schonhagearam@gmail.com>
2024-10-02 21:08:15 +0200

    Added WindowDragger Logging

--- src/main/java/BlueNode/Listener/WindowDragger.java
+++ src/main/java/BlueNode/Listener/WindowDragger.java
@@ -1,5 +1,7 @@
 package BlueNode.Listener;
 
+import BlueNode.Logging.BaseLogger;
+import BlueNode.Logging.ELogLevel;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
@@ -12,11 +14,13 @@ public class WindowDragger {
         pTitleBar.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
+            BaseLogger.log(ELogLevel.DEBUG, "WindowDragger" + "xOffset: " + xOffset + " yOffset: " + yOffset);
         });
 
         pTitleBar.setOnMouseDragged(pEvent -> {
             pStage.setX(pEvent.getScreenX() - xOffset);
             pStage.setY(pEvent.getScreenY() - yOffset);
+            BaseLogger.log(ELogLevel.DEBUG, "WindowDragger" + "x: " + pStage.getX() + " y: " + pStage.getY());
         });
     }
 }
