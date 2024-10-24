3690b72272cbf5bdda00f68bb847d6a92a61d427
Aram <schonhagearam@gmail.com>
2024-10-24 10:16:44 +0200

    Outsourced the Events to another file

--- src/main/java/BlueNode/Nodes/Events/NodeEventHandler.java
+++ src/main/java/BlueNode/Nodes/Events/NodeEventHandler.java
@@ -1,16 +1,29 @@
 package BlueNode.Nodes.Events;
 
+import BlueNode.Logging.BaseLogger;
+import BlueNode.Logging.ELogLevel;
 import BlueNode.Nodes.Types.AbstractNode;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
 public class NodeEventHandler {
-    public static void handleKeyPress(AbstractNode node, KeyCode code) {
-        if (node.isSelected() && (code == KeyCode.DELETE || code == KeyCode.BACK_SPACE)) {
-            Pane parentPane = (Pane) node.getNodePane().getParent();
+    public static void handleSelect(AbstractNode pNode) {
+        if (AbstractNode.getCurrentlySelectedNode() != null && AbstractNode.getCurrentlySelectedNode() != pNode) {
+            AbstractNode.getCurrentlySelectedNode().deselect();
+        }
+
+        if (AbstractNode.getCurrentlySelectedNode() != pNode) {
+            pNode.select();
+            AbstractNode.setCurrentlySelectedNode(pNode);
+        }
+    }
+
+    public static void handleKeyPress(AbstractNode pNode, KeyCode pCode) {
+        if (pNode.isSelected() && (pCode == KeyCode.DELETE || pCode == KeyCode.BACK_SPACE)) {
+            Pane parentPane = (Pane) pNode.getNodePane().getParent();
             if (parentPane != null) {
-                parentPane.getChildren().remove(node.getNodePane());
-                // Perform any additional cleanup or logging
+                parentPane.getChildren().remove(pNode.getNodePane());
+                BaseLogger.log(ELogLevel.INFO,"Node removed from parent pane.");
             }
         }
     }
--- src/main/java/BlueNode/Nodes/Types/AbstractNode.java
+++ src/main/java/BlueNode/Nodes/Types/AbstractNode.java
@@ -2,9 +2,9 @@ package BlueNode.Nodes.Types;
 
 import BlueNode.Logging.BaseLogger;
 import BlueNode.Logging.ELogLevel;
+import BlueNode.Nodes.Events.NodeEventHandler;
 import BlueNode.Nodes.Interfaces.ISelectable;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
@@ -13,31 +13,25 @@ public abstract class AbstractNode implements ISelectable {
     private boolean isSelected;
     private static AbstractNode currentlySelectedNode = null;
 
+    public static AbstractNode getCurrentlySelectedNode() {
+        return currentlySelectedNode;
+    }
+
+    public static void setCurrentlySelectedNode(AbstractNode pNode) {
+        currentlySelectedNode = pNode;
+    }
+
     public AbstractNode() {
         nodePane = new Pane();
         isSelected = false;
 
         nodePane.setOnMouseClicked(event -> {
-            if (currentlySelectedNode != null && currentlySelectedNode != this) {
-                currentlySelectedNode.deselect();
-            }
-
-            if (currentlySelectedNode != this) {
-                this.select();
-                currentlySelectedNode = this;
-            }
+            NodeEventHandler.handleSelect(this);
         });
 
         nodePane.setOnKeyPressed(event -> {
             BaseLogger.log(ELogLevel.DEBUG, "Key pressed: " + event.getCode());
-            if (isSelected && (event.getCode() == KeyCode.DELETE || event.getCode() == KeyCode.BACK_SPACE)) {
-                Pane parentPane = (Pane) nodePane.getParent();
-                if (parentPane != null) {
-                    BaseLogger.log(ELogLevel.INFO, "Node removed");
-                    parentPane.getChildren().remove(nodePane);
-                    currentlySelectedNode = null;
-                }
-            }
+            NodeEventHandler.handleKeyPress(this, event.getCode());
         });
 
         nodePane.setFocusTraversable(true);
