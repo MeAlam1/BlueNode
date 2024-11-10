04ab2f786584ced63edbd9261ae95ce04e85a3b0
Aram <schonhagearam@gmail.com>
2024-11-10 21:55:52 +0100

    Added my Logging

---
+++ utils/src/main/java/com/mealam/bluenode/utils/logging/DefaultLogColorProvider.java
@@ -0,0 +1,50 @@
+package com.mealam.bluenode.utils.logging;
+
+import com.mealam.bluenode.utils.interfaces.logging.ILogColorProvider;
+
+import java.util.logging.Level;
+
+/**
+ * A {@code class} that implements the {@link ILogColorProvider} interface
+ * to provide color codes for different log levels.
+ * <p>
+ * This implementation uses predefined colors for various log levels, including:
+ * <ul>
+ *   <li>{@link BaseLogLevel#ERROR} - Red color.</li>
+ *   <li>{@link BaseLogLevel#WARNING} - Orange color.</li>
+ *   <li>{@link BaseLogLevel#INFO} - Blue color.</li>
+ *   <li>{@link BaseLogLevel#SUCCESS} - Green color.</li>
+ *   <li>{@link BaseLogLevel#DEFINITE} - Green color.</li>
+ * </ul>
+ *
+ * @author MeAlam
+ * @since 1.0.0
+ */
+public class DefaultLogColorProvider implements ILogColorProvider {
+
+    /**
+     * A {@link String} that provides the color code for a given {@link Level}.
+     * The color code is determined based on the log level.
+     *
+     * @param pLevel {@link Level} - The log level for which to determine the color.
+     * @return The color code associated with the {@code pLevel}.
+     * @author MeAlam
+     * @since 1.0.0
+     */
+    @Override
+    public String getColor(Level pLevel) {
+        if (pLevel == BaseLogLevel.ERROR) {
+            return LoggerConfig.RED;
+        } else if (pLevel == BaseLogLevel.WARNING) {
+            return LoggerConfig.ORANGE;
+        } else if (pLevel == BaseLogLevel.INFO) {
+            return LoggerConfig.BLUE;
+        } else if (pLevel == BaseLogLevel.SUCCESS) {
+            return LoggerConfig.GREEN;
+        } else if (pLevel == BaseLogLevel.DEFINITE) {
+            return LoggerConfig.GREEN;
+        } else {
+            return LoggerConfig.RESET;
+        }
+    }
+}
