4802a69cae46f00342d98533a688c8db630d7eb7
Aram <schonhagearam@gmail.com>
2024-11-12 16:35:19 +0100

    Improved Gradle for Testing

---
+++ ui/src/test/java/com/mealam/bluenode/UITest.java
@@ -0,0 +1,12 @@
+package com.mealam.bluenode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UITest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
